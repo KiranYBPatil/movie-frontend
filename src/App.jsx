80c8aeca09802896c1eb55bc835d54006000f976
KiranYBPatil <kiranpatil8660519@gmail.com>
2025-12-28 19:15:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import AIAssistant from "./AIAssistant";
 
-function App() {
-  return (
-    <div>
-      <h1>🎥 BookMyShow Clone</h1>
 
-      <div style={{ position: "fixed", bottom: 20, right: 20 }}>
-        <AIAssistant />
-      </div>
-    </div>
-  );
+function App() {
+  return <AIAssistant />;
 }
 
 export default App;
